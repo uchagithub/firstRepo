51ba6f9a34578660ca8adb089cccfb4cb32cddf4
ucha <uchaokwudili63@gmail.com>
2022-04-08 11:10:59 +0100

    Another commit

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-console.log(firstRepo);
-if (true){
-let mark = 5};
-let num1 =7;
-let bet = 6;
+var numbersArray = [60, 50, 62, 58, 54, 54];
+let array2 =numbersArray;
+function compareNumbers(num1, num2) {
+    if (num1 > num2) {
+    return 1;
+    } else if (num1 === num2) {
+    return 0;
+    } else {
+    return -1;
+    }
+    }
+    let modifiednumbersArray=numbersArray.sort(compareNumbers);
+    console.log(modifiednumbersArray);
+    console.log(numbersArray);
+    function compareNumbers1(num1, num2) {
+        return num2 * num1;
+        }
+        console.log(compareNumbers1);
+        array2.sort(compareNumbers1);
+        console.log(array2);
